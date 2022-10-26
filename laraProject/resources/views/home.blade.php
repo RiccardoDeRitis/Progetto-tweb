d6c89190323c4e3fe0710afafa42ba6314c9b305
RiccardoDeRitis <s1094657@studenti.univpm.it>
2022-10-26 18:31:36 +0200

    daje
    daje

--- laraProject/resources/views/home.blade.php
+++ laraProject/resources/views/home.blade.php
@@ -9,11 +9,31 @@
         <p>Stringi nuove amicizie pubblicando, commentando nuovi post</p>
     </div>
     @auth
-    <h1>MOST POPULAR BLOG</h1>
-    @foreach($blogs as $blog)
-            <h2>{{$blog->Titolo}}</h2>
-            <h3>{{$blog->Descrizione}}</h3>
-    @endforeach
+    <div style="background-color: rgb(232, 253, 255);">
+        <h1 class="blog_data" style="margin-left: 43.5%; font-size: 36px; font-weight: bold;">Most Popular Blog</h1>
+        <table class="table_blog" style="background-color: rgb(232, 253, 255);">
+            <tr style="height: 50%">
+            @for ($i = 0;$i < count($blogs); $i++)
+                @if ($i%3 == 0)
+                    </tr>
+                    <tr style="height: 50%">
+                        <td style="width: 35%; height: 100%;">
+                            <div class="container_blog" style="width: 80%; height: 100%;"> 
+                                <h1 class="blog_data"> Titolo: {{$blogs[$i]->Titolo}} </h1> 
+                                <h1 class="blog_data" style="width: 80%"> Descrizione: {{$blogs[$i]->Descrizione}} </h1> 
+                            </div>
+                        </td>
+                @else
+                    <td style="width: 35%; height: 100%;">
+                        <div class="container_blog" style="width: 80%; height: 100%;"> 
+                            <h1 class="blog_data"> Titolo: {{$blogs[$i]->Titolo}} </h1> 
+                            <h1 class="blog_data" style="width: 80%"> Descrizione: {{$blogs[$i]->Descrizione}} </h1> 
+                        </div>
+                    </td>
+                @endif
+            @endfor
+        </table>
+    </div>
     @endauth
     <div class="relative">
         <div class="absolute" style="margin-left: 50%">
