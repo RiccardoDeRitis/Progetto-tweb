93428696af41b0a11448e17e4247e7f34d60d4b9
RiccardoDeRitis <s1094657@studenti.univpm.it>
2022-10-26 18:21:19 +0200

    tonino
    tonino

--- laraProject/resources/views/miei_blog.blade.php
+++ laraProject/resources/views/miei_blog.blade.php
@@ -3,17 +3,49 @@
 
 @section('content')
 
+<h1 class="title_profile"> I tuoi blog </h1>
+<hr size="3" align="center" width="90%" color="#008CBA" noshade>
+
 <div id="Mod_Container">
     <a href="{{ route('crea_blog') }}">
         <button><i aria-hidden="true"></i> Crea Blog </button>
     </a>
 </div>
 
+<table class="table_blog">
+    <tr style="height: 50%">
+    @for ($i = 0;$i < count($blogs); $i++)
+        @if ($i%3 == 0)
+            </tr>
+            <tr style="height: 50%">
+                <td style="width: 35%; height: 100%;">
+                    <div class="container_blog" style="width: 80%; height: 100%;"> 
+                        <h1 class="blog_data"> Titolo: {{$blogs[$i]->Titolo}} </h1> 
+                        <h1 class="blog_data" style="width: 80%"> Descrizione: {{$blogs[$i]->Descrizione}} </h1> 
+                    </div>
+                </td>
+        @else
+            <td style="width: 35%; height: 100%;">
+                <div class="container_blog" style="width: 80%; height: 100%;"> 
+                    <h1 class="blog_data"> Titolo: {{$blogs[$i]->Titolo}} </h1> 
+                    <h1 class="blog_data" style="width: 80%"> Descrizione: {{$blogs[$i]->Descrizione}} </h1> 
+                </div>
+            </td>
+        @endif
+    @endfor
+</table>
 
-@foreach($blogs as $blog)
-<h1>{{$blog->Titolo}}</h1>
-<h2>{{$blog->Descrizione}}</h2>
-
-@endforeach
+<!-- <script>
+    document.write("<table>");
+    document.write("<tr>");
+    for(var i=0; i < count($blogs); i++) {
+        if (i%3 == 0) {
+            document.write("</tr><tr><td> ciao </td>");
+        } else {
+            document.write("<td> ciao </td>");
+        }
+    }   
+    document.write("</table>");
+</script> -->
 
 @endsection
