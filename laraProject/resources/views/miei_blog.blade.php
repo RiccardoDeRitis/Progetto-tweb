53285cbafbe202b9a8cd735fb8bdd1eddd167f5f
RiccardoDeRitis <s1094657@studenti.univpm.it>
2022-10-30 17:53:03 +0100

    tonino
    tonino

--- laraProject/resources/views/miei_blog.blade.php
+++ laraProject/resources/views/miei_blog.blade.php
@@ -12,20 +12,20 @@
         @if ($i%3 == 0)
             </tr>
             <tr>
-                <td style="width: 33%; height: 300px;">
-                    <div class="container_blog" style="width: 80%; height: 80%;"> 
+                <td class="td_container">
+                    <div class="container_blog"> 
                         <a href="{{route('blog', [$blogs[$i]->IDBlog])}}" style="text-decoration: none; color: black;">
-                            <h1 class="blog_data" style="padding-top: 10%"> Titolo: {{$blogs[$i]->Titolo}} </h1> 
-                            <h1 class="blog_data" style="width: 80%"> Descrizione: {{$blogs[$i]->Descrizione}} </h1> 
+                            <h1 class="blog_data" style="padding-top: 10%; font-style: italic;"> <b>Titolo :</b> {{$blogs[$i]->Titolo}} </h1> 
+                            <h1 class="blog_data" style="width: 80%; font-style: italic;"> <b>Descrizione :</b> {{$blogs[$i]->Descrizione}} </h1> 
                         </a>
                     </div>
                 </td>
         @else
-            <td style="width: 33%; height: 300px;">
-                <div class="container_blog" style="width: 80%; height: 80%;"> 
+            <td class="td_container">
+                <div class="container_blog"> 
                     <a href="{{route('blog', [$blogs[$i]->IDBlog])}}" style="text-decoration: none; color: black; padding-top: 16%;">
-                        <h1 class="blog_data" style="padding-top: 10%"> Titolo: {{$blogs[$i]->Titolo}} </h1> 
-                        <h1 class="blog_data" style="width: 80%"> Descrizione: {{$blogs[$i]->Descrizione}} </h1> 
+                        <h1 class="blog_data" style="padding-top: 10%; font-style: italic;"> <b>Titolo :</b> {{$blogs[$i]->Titolo}} </h1> 
+                        <h1 class="blog_data" style="width: 80%; font-style: italic;"> <b>Descrizione :</b> {{$blogs[$i]->Descrizione}} </h1> 
                     </a>
                 </div>
             </td>
@@ -34,16 +34,16 @@
             @if (($i+1)%3 == 0)
                 </tr>
                 <tr>
-                    <td style="width: 33%; height: 300px;">
-                        <div class="container_blog" style="width: 80%; height: 80%; text-align: center;">
+                    <td class="td_container">
+                        <div class="container_blog" style="text-align: center;">
                             <a href="{{ route('crea_blog') }}">
                                 <span aria-hidden="true"><i class="fa fa-plus" style="font-size: 80px; padding-top: 16%;"></i></span>
                             </a>
                         </div>
                     </td>
             @else
-                <td style="width: 33%; height: 300px;">
-                    <div class="container_blog" style="width: 80%; height: 80%; text-align: center;">
+                <td class="td_container">
+                    <div class="container_blog" style="text-align: center;">
                         <a href="{{ route('crea_blog') }}" style="text-decoration: none; color: black;">
                             <span aria-hidden="true"><i class="fa fa-plus" style="font-size: 80px; padding-top: 16%;"></i></span>
                         </a>
